087e5bc9d27b1f7fc3244b4f3c072143a7b1f5fd
Junuk Cha <jucha@unist.ac.kr>
2024-04-29 16:06:34 -0700

    Update test_seq2seqL1_custom.py

--- experiments/t2onet/test_seq2seqL1_custom.py
+++ experiments/t2onet/test_seq2seqL1_custom.py
@@ -9,6 +9,7 @@ import utils.html as html
 from utils.visualize import update_web
 from options.seq2seqGAN_train_options import TrainOptions
 from datasets.FiveKdataset import FiveK
+from datasets.Customdataset import CustomDataset
 from models.actor import Actor
 from utils.eval import ImageEvaluator
 from utils.text_utils import load_vocab, txt2idx
@@ -53,9 +54,11 @@ def test(model, loader, opt, is_test=False):
         # for i in range(1000):
         itr += 1
         tik = time.time()
-        img_x, img_y, x, req = data
+        img_x, img_y, txt = data
         # ship to device
-        x, img_x, img_y = list(map(lambda r: r.to(device), [x, img_x, img_y]))
+        img_x, img_y = list(map(lambda r: r.to(device), [img_x, img_y]))
+        vocab2id, _, _, _ = load_vocab(opt.vocab_dir, opt.dataset, opt.session)
+        x = txt2idx(txt, vocab2id, opt.encoder_max_len).to(device)
         with torch.no_grad():
             state, pred_imgs, pred_ops, pred_params = model.episode_forward(x, img_x, mask_dict=None,
                                                                                         reinforce_sample=False)
@@ -68,27 +71,27 @@ def test(model, loader, opt, is_test=False):
             end_fake_imgs.append(pred_imgs[bs_i, col_idx])
         pred_img = torch.stack(end_fake_imgs)
 
-        tok = time.time()
-        avg_time = avg_time * (1 - 1/itr) + (tok - tik) / itr
-        init_dist = torch.abs(img_x - img_y).mean().item()
-        dist = torch.abs(pred_img - img_y).mean().item()
-        avg_init_dist = avg_init_dist * (1 - 1/itr) + init_dist/itr
-        avg_dist = avg_dist * (1 - 1 / itr) + dist / itr
+        # tok = time.time()
+        # avg_time = avg_time * (1 - 1/itr) + (tok - tik) / itr
+        # init_dist = torch.abs(img_x - img_y).mean().item()
+        # dist = torch.abs(pred_img - img_y).mean().item()
+        # avg_init_dist = avg_init_dist * (1 - 1/itr) + init_dist/itr
+        # avg_dist = avg_dist * (1 - 1 / itr) + dist / itr
 
-        if is_test:
-            Eval.update(img_x, pred_img, img_y)
+        # if is_test:
+        #     Eval.update(img_x, pred_img, img_y)
 
-        if itr % opt.print_every == 0:
-            print('iter {:6d} / {}, init dist {:.2f},  L1 dist {:.2f} time {:.2f}'.format(itr, len(loader), init_dist, dist, avg_time))
+        # if itr % opt.print_every == 0:
+        #     print('iter {:6d} / {}, init dist {:.2f},  L1 dist {:.2f} time {:.2f}'.format(itr, len(loader), init_dist, dist, avg_time))
 
-        if opt.visualize and itr % opt.visualize_every == 0:
-            pred_params = torch.cat([pred_params[i][:, :1] for i in range(len(pred_params))]).unsqueeze(0)
-            pdb.set_trace()
+        # if opt.visualize and itr % opt.visualize_every == 0:
+        #     pred_params = torch.cat([pred_params[i][:, :1] for i in range(len(pred_params))]).unsqueeze(0)
+        #     pdb.set_trace()
     
     results_folder = "results"
     os.makedirs(results_folder, exist_ok=True)
     save_image(img_x[0], os.path.join(results_folder, "input.jpg"))
-    save_image(img_y[0], os.path.join(results_folder, "target.jpg"))
+    # save_image(img_y[0], os.path.join(results_folder, "target.jpg"))
     save_image(pred_img[0], os.path.join(results_folder, "pred.jpg"))
     
     
@@ -160,7 +163,7 @@ if __name__ == '__main__':
     # data loader
     img_dir = 'data/FiveK/images'
     anno_dir = 'data/FiveK/annotations'
-    test_dataset = FiveK(img_dir, anno_dir, opt.vocab_dir, 'test', opt.session)
+    test_dataset = CustomDataset("../code/data/s3_json/test/test_s3_data_small2.json", "junukc", "jaeyoon-west2")
     loader = DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers)
     # load model
     model = Actor(opt)
