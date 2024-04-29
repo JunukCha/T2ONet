92d051e86601cd9103de397ed1c8e77f0cf7eea2
Junuk Cha <jucha@unist.ac.kr>
2024-04-29 15:33:39 -0700

    Update lang_encoder.py

--- models/lang_encoder.py
+++ models/lang_encoder.py
@@ -91,6 +91,8 @@ class RNNEncoder(nn.Module):
         embedded = self.embedding(input_labels)  # (n, seq_len, word_embedding_size)
         embedded = self.input_dropout(embedded) # (n, seq_len, word_embedding_size)
         if self.variable_lengths:
+            sorted_input_lengths = sorted_input_lengths.cpu()
+            embedded = embedded.cpu()
             print("sorted_input_lengths", sorted_input_lengths.device)
             print("embedded", embedded.device)
             embedded = nn.utils.rnn.pack_padded_sequence(embedded, sorted_input_lengths, batch_first=True)
