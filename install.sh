8e63b88a614b9253b31e06c3722b119813d17ee0
Junuk Cha <jucha@unist.ac.kr>
2024-04-29 16:09:28 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+conda create -n LGIE python=3.10 -y
+conda activate LGIE
+
+pip install opencv-python 
+pip install dominate
+pip install kornia==0.5.0
+pip install scipy
+pip install scikit-image
+pip install torch==2.0.1 torchvision==0.15.2
