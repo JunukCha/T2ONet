9f23818a04464d3b3d33f5b61938630d532ef77e
Junuk Cha <jucha@unist.ac.kr>
2024-05-01 14:34:06 -0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,1 +1,1 @@
-python experiments/t2onet/test_seq2seqL1_custom.py --print_every 100 --visualize_every 100  --visualize 0 --is_train 0 --trial 1
+PYTHONPATH='.' python experiments/t2onet/test_seq2seqL1_custom.py --print_every 100 --visualize_every 100  --visualize 0 --is_train 0 --trial 1
